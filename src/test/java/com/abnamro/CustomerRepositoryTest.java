fbc5c5f5c9e0530cffc243e67fcb4beb2d9adb9e
Assen Kolov <assen.kolov@gmail.com>
2015-12-02 10:58:41 +0100

    Docker

--- src/test/java/com/abnamro/CustomerRepositoryTest.java
+++ src/test/java/com/abnamro/CustomerRepositoryTest.java
@@ -6,13 +6,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.WebIntegrationTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
- 
- 
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = AbndemoApplication.class)
-
+@WebIntegrationTest // needed after adding swagger 
 public class CustomerRepositoryTest {
 
 	@Autowired
@@ -23,13 +22,11 @@ public class CustomerRepositoryTest {
 		Assert.assertThat(repository.findByLastName("Kumar").size(), CoreMatchers.equalTo(0));
 
 		repository.save(new Customer("Assen", "Kumar"));
-		
+
 		Assert.assertThat(repository.findByLastName("Kumar").size(), CoreMatchers.equalTo(1));
 
 		Assert.assertThat(repository.findByLastName("Costanza").size(), CoreMatchers.equalTo(2));
 
 	}
-
-
 
 }
--- src/test/java/com/abnamro/HelloControllerTest.java
+++ src/test/java/com/abnamro/HelloControllerTest.java
@@ -14,20 +14,19 @@ import org.springframework.web.client.RestTemplate;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = AbndemoApplication.class)
-@WebIntegrationTest(randomPort=true)
+@WebIntegrationTest(randomPort = true)
 public class HelloControllerTest {
 
-	private RestTemplate restTemplate = new TestRestTemplate( "abn", "abn");
-	
-	@Value("${local.server.port}") 
-	private String port;
+	private RestTemplate restTemplate = new TestRestTemplate("abn", "abn");
 
+	@Value("${local.server.port}")
+	private String port;
 
 	@Test
 	public void testHello() throws Exception {
-		ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:" + port +"/hello",
+		ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:" + port + "/hello",
 				String.class);
-		Assert.assertThat(response.getBody(), CoreMatchers.startsWith("hello" ));
+		Assert.assertThat(response.getBody(), CoreMatchers.startsWith("hello"));
 	}
 
 }
